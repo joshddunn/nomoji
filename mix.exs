71422df64815c228f11ea476e7e60823ca9a4da9
joshddunn <joshdunn.uwo@gmail.com>
2019-03-29 02:11:52 -0400

    add info for hex package

--- mix.exs
+++ mix.exs
@@ -7,22 +7,35 @@ defmodule Nomoji.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Nomoji",
+      source_url: "https://github.com/joshddunn/nomoji"
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
+    []
+  end
+
+  defp description() do
+    "An elixir package that will remove emojis from your strings."
+  end
+
+  defp package do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      name: "nomoji",
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/joshddunn/nomoji"}
     ]
   end
 end
